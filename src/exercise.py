373d197c10d6820816895a177513592db1676cca
Jake Lee <jake.lee@fdmgroup.com>
2021-10-14 13:16:44 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
+from personal_information import PersonalInformation
+
 def main():
     #write your code below this line
+    people = []
+    while True:
+        first = input("First Name:")
+        if not first:
+            break
+        last = input("Last Name:")
+        id = input("Identification Number:")
+        person = PersonalInformation(first, last, id)
+        people.append(person)
+    
 
 if __name__ == '__main__':
     main()
